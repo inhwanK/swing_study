dda16173891c49d2eb169292f1c87205590b8ec6
inhwanK <inhan1009@naver.com>
2021-03-04 19:16:12 +0900

    텍스트 필드  DISPOSE_ON_CLOSE

--- src/main/java/swing_study/component/JTextFieldEx.java
+++ src/main/java/swing_study/component/JTextFieldEx.java
@@ -41,7 +41,7 @@ public class JTextFieldEx extends JFrame implements ActionListener {
 
 	private void initialize() {
 		setPreferredSize(new Dimension(450, 500));
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 565);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
